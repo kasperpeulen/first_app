e11a3a7e516626664bada24db13e326130352a54
Kasper Peulen <kasperpeulen@gmail.com>
2015-10-26 13:28:28 +0100

    Initial Commit

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Kasper Peulen on 10/26/15.
+//  Copyright © 2015 Kasper Peulen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
