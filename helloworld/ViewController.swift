130368fd7200343a2d46725b10c1ce024ed44110
Kasper Peulen <kasperpeulen@gmail.com>
2015-10-26 15:11:18 +0100

    initial commit

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func sendName(sender: AnyObject) {
+        nameLabel.text = "Hello, \(textField.text!)"
+    }
 }
-
